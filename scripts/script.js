d279a7e6f32cd9adb854fba66a8a07cb1be5b2e2
edsonarts <edsonarts961@gmail.com>
2024-09-01 08:08:17 +0200

    Update script.js
    updated pics

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const images = [
     'images/image2.jpeg',
     'images/image3.jpeg',
     'images/image4.jpeg',
-    // '/images/image5.jpeg',
+    '/images/image5.jpeg',
     // '/images/image6.jpeg',
     // '/images/image7.jpeg',
     // '/images/image8.jpeg',
