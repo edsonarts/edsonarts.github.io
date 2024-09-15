425f9879b1f7e74a37ae7031f79b856777730bf6
edsonarts <edsonarts961@gmail.com>
2024-09-15 22:42:47 +0200

    Update script.js
    updated images

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,27 @@ const images = [
     'images/image3.jpeg',
     'images/image4.jpeg',
     '/images/image5.jpeg',
-    // '/images/image6.jpeg',
-    // '/images/image7.jpeg',
-    // '/images/image8.jpeg',
-    // '/images/image9.jpeg',
-    // '/images/image10.jpeg',
-    // '/images/image11.jpeg',
-    // '/images/image12.jpeg',
-    // '/images/image13.jpeg',
-    // '/images/image14.jpeg',
-    // '/images/image15.jpeg'
+    '/images/image6.jpeg',
+    '/images/image7.jpeg',
+    '/images/image8.jpeg',
+    '/images/image9.jpeg',
+    '/images/image10.jpeg',
+    '/images/image11.jpeg',
+    '/images/image12.jpeg',
+    '/images/image13.jpeg',
+    '/images/image14.jpeg',
+    '/images/image15.jpeg'
+    '/images/image16.jpeg',
+    '/images/image17.jpeg',
+    '/images/image18.jpeg',
+    '/images/image19.jpeg',
+    '/images/image20.jpeg',
+    '/images/image21.jpeg',
+    '/images/image22.jpeg',
+    '/images/image23.jpeg',
+    '/images/image24.jpeg',
+    '/images/image25.jpeg'
+    '/images/image27.jpeg'
     // Add more image paths as needed
 ];
 
